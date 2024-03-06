c01d5440a96d2d1961cfa0233d61a46ebc017de0
Enkutatash <enkutatasheshetu96.com>
2024-03-06 23:41:05 +0300

    UI on front page profile,searchbar and bottom navigation

---
+++ capstone_project/ecommerce/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce/pages/HomePage.dart';
+void main() {
+  runApp( MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
